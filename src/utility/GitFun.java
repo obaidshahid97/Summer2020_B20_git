e4ded7db4d8e97047af3f5547b55b88fc43cedcd
obaidshahid97 <69281785+obaidshahid97@users.noreply.github.com>
2020-08-05 21:32:47 -0400

    Update GitFun.java

--- src/utility/GitFun.java
+++ src/utility/GitFun.java
@@ -3,6 +3,9 @@ package utility;
 public class GitFun {
     public static void main(String[] args) {
         System.out.println("Everything is fun");
+        
+        
+        // this line is edit from GIthub
     }
 
 
